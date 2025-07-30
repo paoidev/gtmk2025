0c8f73a41b44cd69ce618e8c5ba59ced320421b6
Paoi <paoi.uy.dev@gmail.com>
2025-07-31 04:58:37 +0800

    feat: third person camera
    - freelook camera
    - player moves with direction

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -5,15 +6,25 @@ public class PlayerController : MonoBehaviour
     [Header("References")]
     [SerializeField] private PlayerInputs inputs;
     [SerializeField] private CharacterController controller;
+    [SerializeField] private CinemachineCamera CMCam;
+    [SerializeField] private Camera cam;
+    [SerializeField] CinemachineOrbitalFollow orbitalFollow;
 
     [Header("Movement settings")]
     public float baseMoveSpeed = 5f;
     public float gravity = 30f;
     public float jumpHeight = 5f;
+    public float turnSpeed = 10f;
+
+    [Header("Camera settings")]
+    private Vector3 cameraForward;
+    private Vector3 cameraRight;
 
     [Header("Stored values")]
+    [SerializeField] private Vector3 movementMagnitude;
     public float verticalVelocity;
     public bool isGrounded = false;
+    public bool isMoving = false;
     public bool isSprinting = false;
     public bool isJumping = false;
 
@@ -25,16 +36,38 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        cameraForward = Camera.main.transform.forward;
+        cameraRight = Camera.main.transform.right;
+
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+
         Move();
     }
 
     private void Move()
     {
-        Vector3 move = (transform.forward * inputs.MoveInput.y + transform.right * inputs.MoveInput.x);
+        Vector3 move = (cameraForward * inputs.MoveInput.y + cameraRight * inputs.MoveInput.x);
         move *= baseMoveSpeed;
         move.y = VerticalMovement();
 
         controller.Move(move * Time.deltaTime);
+
+        #region Debug
+        Vector3 horizontalMove = new Vector3(move.x, 0f, move.z);
+        isMoving = horizontalMove.magnitude > 0.1f;
+        movementMagnitude = move;
+        #endregion
+
+        if (isMoving)
+        {
+            Vector3 lookDirection = new Vector3(move.x, 0f, move.z);
+            Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+
+        }
     }
 
     private float VerticalMovement()
@@ -53,5 +86,13 @@ public class PlayerController : MonoBehaviour
         }
         
         return verticalVelocity;
+    }
+
+    private void OnApplicationFocus(bool hasFocus)
+    {
+        if (hasFocus)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
     }
 }
