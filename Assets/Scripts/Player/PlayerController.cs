889ecc41ff52aa21cfeda13709e21dd67012062f
Paoi <paoi.uy.dev@gmail.com>
2025-07-31 04:37:48 +0800

    feat: move, gravity, jump
    - basic movement
    - no sprint yet
    - import probuilder and cinemachine

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,15 +2,56 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("References")]
+    [SerializeField] private PlayerInputs inputs;
+    [SerializeField] private CharacterController controller;
+
+    [Header("Movement settings")]
+    public float baseMoveSpeed = 5f;
+    public float gravity = 30f;
+    public float jumpHeight = 5f;
+
+    [Header("Stored values")]
+    public float verticalVelocity;
+    public bool isGrounded = false;
+    public bool isSprinting = false;
+    public bool isJumping = false;
+
     void Start()
     {
-        
+        inputs = GetComponent<PlayerInputs>();    
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 move = (transform.forward * inputs.MoveInput.y + transform.right * inputs.MoveInput.x);
+        move *= baseMoveSpeed;
+        move.y = VerticalMovement();
+
+        controller.Move(move * Time.deltaTime);
+    }
+
+    private float VerticalMovement()
+    {
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -3f;
+            if (inputs.JumpInput)
+            {
+                verticalVelocity = Mathf.Sqrt(jumpHeight * 2 * gravity);
+            }
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
         
+        return verticalVelocity;
     }
 }
--- Assets/Scripts/Player/PlayerInputs.cs
+++ Assets/Scripts/Player/PlayerInputs.cs
@@ -1,16 +1,39 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInputs : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Vector2 MoveInput { get; private set; }
+    public bool JumpInput { get; private set; }
+    public bool SprintInput { get; private set; }
+    
+    public void OnMove(InputAction.CallbackContext context)
     {
-        
+        MoveInput = context.ReadValue<Vector2>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnJump(InputAction.CallbackContext context)
     {
-        
+        if (context.performed)
+        {
+            JumpInput = true;
+        }
+        else
+        {
+            JumpInput = false;
+        }
+    }
+
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            SprintInput = true;
+        }
+        else
+        {
+            SprintInput = false;
+        }
     }
 }
